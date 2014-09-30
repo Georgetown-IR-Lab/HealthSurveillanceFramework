20f2f468fee596692694e9bedc8aac6624968770
Andrew Yates <andrew.yates@gmail.com>
2014-09-29 20:10:36 -0700

    factor out date aggregation

--- component/trend_detection/simple_weekly.py
+++ component/trend_detection/simple_weekly.py
@@ -1,4 +1,3 @@
-import calendar
 import codecs
 import datetime
 import json
@@ -27,18 +26,9 @@ class WeeklyTrendDetection(TrendDetection):
                     trends[concept][date] = 1
                 else:
                     trends[concept][date] += 1
-
+        
         # output trends
-        trendsout = {}
-        for concept, datecounts in trends.iteritems():
-            if concept not in trendsout:
-                trendsout[concept] = []
-
-            for date, count in datecounts.iteritems():
-                week = isoweek.Week(date.year, date.isocalendar()[1]).monday()
-                unixtime = calendar.timegm(week.timetuple())
-                trendsout[concept].append({"start": unixtime, "end": unixtime, "strength": count})
-
+        trendsout = self._aggregate_trends(trends, lambda date: isoweek.Week(date.year, date.isocalendar()[1]).monday())
         json.dump(trendsout, codecs.open(self.outfn, "w", encoding="utf-8"))
 
 if __name__ == "__main__":
--- framework/trend_detection.py
+++ framework/trend_detection.py
@@ -1,5 +1,6 @@
 import codecs
 import json
+import time
 
 from abc import ABCMeta, abstractmethod
 
@@ -26,3 +27,21 @@ class TrendDetection(object):
     def run(self):
         """ Runs component and writes output to self.outfn """
         pass
+
+    def _aggregate_trends(self, trends, aggregatef):
+        """ Aggregate document counts from different days by calling aggregatef on each date. Args:
+        trends: a dictionary mapping concepts to dates to counts
+        aggregatef: a function taking a datetime object and returning a new datetime object at some level of aggregation
+        """
+
+        agg_trends = {}
+        for concept, datecounts in trends.iteritems():
+            if concept not in agg_trends:
+                agg_trends[concept] = {}
+
+            for date, count in datecounts.iteritems():
+                unixtime = time.mktime(aggregatef(date).timetuple())
+                agg_trends[concept][unixtime] = agg_trends[concept].get(unixtime, 0) + count
+
+        return {concept: [{"start": unixtime, "end": unixtime, "strength": count} for unixtime, count in times.iteritems()]
+                for concept, times in agg_trends.iteritems()}
